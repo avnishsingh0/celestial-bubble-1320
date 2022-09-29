8c8c1062ce3888ad31653de30b97fa18fcc5b895
surya2697 <107541204+surya2697@users.noreply.github.com>
2022-09-29 22:57:02 +0530

    Add files via upload

---
+++ smartphonedata.js
@@ -0,0 +1,352 @@
+var Gphones=[
+    {
+        image:"https://www.reliancedigital.in/medias/Motorola-G51-PAS80007IN-SMART-PHONE-492849093-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMjMzNHxpbWFnZS9qcGVnfGltYWdlcy9oOWYvaGFmLzk3ODYyMzk2MTUwMDYuanBnfGUwYWQ4MjI2MDc0OTI1ZDYyMmYxNjk0ZjllZGYyYTUyNjQ3MmZjNmMzNGY1ZjM4NDUxMGFjYzNhMWYwZDZjOWM",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/One-Plus-10R-Mobile-Phone-492850198-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMDAwOHxpbWFnZS9qcGVnfGltYWdlcy9oMWUvaDk5Lzk4NzM5Nzg1MjM2NzguanBnfDM3N2IxOGQ0MmQ2YTZjMmUyZDcwYWRlNmI2MThjMDM5OTYwNDE2NzUyZDQ2M2E5OTYwODczNzU1OTIxYjBlYTM",
+        title:"realme C25Y 64 GB, 4 GB RAM, Glacier Blue, Mobile Phone",
+        price:"32,999",
+        actualprice:"40,999",
+        save:"60%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Motorola-EDGE-30-Mobile-Phone-492912756-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMTU3NXxpbWFnZS9qcGVnfGltYWdlcy9oN2YvaGE2Lzk4MzE2ODY0MDYxNzQuanBnfGM4NWUwNjRiMTgzYTMyM2ZmYWZkN2I0MTNkY2M0ZjdmMTNlZTc3MmZmODI2ZmZmNGFkOWQxMjc2MzRlZDVmZDg",
+        title:"Motorola Moto Edge 30 5G 128GB, 6GB RAM, Meteor Grey, Mobile Phone",
+        price:"15,999",
+        actualprice:"18,999",
+        save:"30%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Vivo-X80-Smartphone-492850419-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w1MDk4OHxpbWFnZS9qcGVnfGltYWdlcy9oNzAvaGQ2Lzk4MzI1ODYyODA5OTAuanBnfDk5ZDNiMTY0MjU0MGViNDc1ZDg0ZDIyZmY4NmEwNWE0MTkxNmI1MWJlNmZiYWZkNjA0N2QxYTU0NmU1MmY3OWM",
+        title:"Vivo X80 5G 128 GB, 8 GB RAM, Cosmic Black, Mobile Phone",
+        price:"11,999",
+        actualprice:"17,999",
+        save:"55%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Redmi-Note-11T-Smartphone-492574112-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDkwNHxpbWFnZS9qcGVnfGltYWdlcy9oNGMvaDQ4Lzk3NzQ0NzczNzc1NjYuanBnfDA0MTBmZTgzNTM2YmIwYTA3YTk1YTNjMWEwYjUwZTMzYTAyOTQ2OTM5Yzk1MGIzMTQ1ZGZiODg2OTI0ZjdhMjA",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"18,999",
+        actualprice:"21,999",
+        save:"40%"
+    },    
+]
+let bestCamera=[
+    {
+        image:"https://www.reliancedigital.in/medias/OnePlus-10-Pro-Smartphone-492849811-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2ODg3NnxpbWFnZS9qcGVnfGltYWdlcy9oMTkvaDY4Lzk4ODIxMDU5MDUxODIuanBnfGRmNWU1MzljZDZjZmE1ZDNlNzJmNTFhOWEzNjg0MTM0OTQ2ODg4ZWFiZDE2NjYwOGI1YzRjZDExN2QzNDYwNGQ",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Oppo-K10-Smartphone-492849759-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2NTYxNHxpbWFnZS9qcGVnfGltYWdlcy9oMTQvaDNmLzk4NzAzMzM1NzUxOTguanBnfDdmNzBhMDNkMDJmZGE1MTBlYzc2MWJhYzQ1OWY0NGFjYzNmYjA5Y2IwNDVhZjQxNGY5ZmFkNmNiZTE4OTYxMDI",
+        title:"realme C25Y 64 GB, 4 GB RAM, Glacier Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Realme-RMX3491-GSMHANDSETSANDROID-492575026-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNjIyNXxpbWFnZS9qcGVnfGltYWdlcy9oZTUvaDhjLzk4MTU4MTkyMjMwNzAuanBnfDBjZjk5MDFkZTE4ZmY1NGVkNzA2Yjc3ZjdhNTI3ZTMwYmY3YmNkY2NjMGJiYTViMmEzNDg2ZDVmNTBmMDZmYjM",
+        title:"Motorola Moto Edge 30 5G 128GB, 6GB RAM, Meteor Grey, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/REDMI-NOTE-11-Mobile-Phone-492849123-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMjgzMHxpbWFnZS9qcGVnfGltYWdlcy9oYTIvaDNmLzk4MjAyOTczMzA3MTguanBnfDBmNDZmOWMzYTkwNTdlOGIyNWQyMzRhOWFkODRiZTg4MGFlZGUxYWQ4MzEyYmNiZTY2MTVjYTcxOTAzOTM2Yjk",
+        title:"Vivo X80 5G 128 GB, 8 GB RAM, Cosmic Black, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Apple-Iphone12-Mobilephones-491901533-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3NzE5fGltYWdlL2pwZWd8aW1hZ2VzL2g3Ny9oNzEvOTg2MzgzNTU4MjQ5NC5qcGd8NDEzYzUzZjg1YmE5ZmVjNDYyNjMxNmY4ODRlNjIyOGM2MDQ1NzRiNGFkYmZjMTlmOTUxMjIzY2MyMWFlMmJlMA",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+]
+let affordabledata=[
+    {
+        image:"https://www.reliancedigital.in/medias/itel-A23Pro-8GB-1GB-RAM-491996691-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNDY1M3xpbWFnZS9qcGVnfGltYWdlcy9oOWEvaGU1Lzk1NDU0MzE4NzU2MTQuanBnfGU3OTIxZGZiMGU0MTBmMTZiMzliYzQzNzhkMTE1Y2UyYmQyNWIwMDYyZThiYjU0NGRhODk3ZDI5Mjk1M2M1ZmI",
+        title:"itel A23 Pro 8 GB, 1 GB RAM, Lake Blue, Mobile Phone",
+        price:"10,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Nokia-G21-Mobile-Phones-492849974-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMDEwN3xpbWFnZS9qcGVnfGltYWdlcy9oMjYvaDRlLzk4NzM5MjkwNDM5OTguanBnfDg0YmY3YjQ5YzJlNTVmMGY5MDA2MjJjNTc4NGFiODIwNGJiYmU3MjZiY2Y1NjJjNDA5NWVmODdjNjQ4ZTRhZTM",
+        title:"realme C25Y 64 GB, 4 GB RAM, Glacier Blue, Mobile Phone",
+        price:"10,999.00",
+        actualprice:"17,999",
+        save:"40%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/VIVO-Y15c-Smartphones-492850290-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2MjI2NnxpbWFnZS9qcGVnfGltYWdlcy9oNTAvaDVhLzk4MzI5NTA1MzAwNzguanBnfGJhZGQ2YTIwNjVjMTZjY2NlMzYyZGMzNWU2OGNkZjI1NTQ3YzNmODVjZWExMmM3YjFiZGE0NjliYTAxMWIxMWY",
+        title:"Itel Vision 2S 32 GB, 2 GB RAM, Deep Blue, Mobile Phone",
+        price:"6,199.00",
+        actualprice:"7,999",
+        save:"20%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Itel-Vision-2S-Smartphones-491997913-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3ODExNHxpbWFnZS9qcGVnfGltYWdlcy9oOWIvaDhlLzk2NTc0MTI0ODUxNTAuanBnfDAzMDdkNjNiZjhiMzg5MjMxMWY1OWZjNTE4ZDcyMDY3Yjc5YjRlNjk5ZmEwMDhlZGZhZGRhMzIzZjliMzA0YTk",
+        title:"Vivo X80 5G 128 GB, 8 GB RAM, Cosmic Black, Mobile Phone",
+        price:"9,499.00",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/realme-C25Y-Smartphones-491998358-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3OTAzNHxpbWFnZS9qcGVnfGltYWdlcy9oY2IvaDFlLzk2NzM1NTUxODE1OTguanBnfDBkZDVhOGU3NDZjNmQ4ZDU0ZjdkNTU3ZGNjYzEwNjllM2E2ZmY1ZmU1Y2UzZjIwNWI1Y2Q0Yzg5MjczOGM0ZWM",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+]
+let gaming=[
+    {
+        image:"https://www.reliancedigital.in/medias/REDMI-NOTE-11-Mobile-Phone-492849130-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMTQ2M3xpbWFnZS9qcGVnfGltYWdlcy9oNjUvaGQxLzk4MjAzMTAxMTAyMzguanBnfDA5ZmEwYjhlZDQzOTgzYzkxMTU4ZjM0NGQxYjdmODFlNDMxMjdjOGE0ZTZmNDZjYmY1MTVmMzc4ZDBkODZhNjg",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Xiaomi-Note-10T-5G-Smartphone-491997375-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNTk4N3xpbWFnZS9qcGVnfGltYWdlcy9oODgvaDI1Lzk4NzkxMjMxNjUyMTQuanBnfGM4MzEwODhjOWI2OTU3MTAzYzBmZTlkMzkzYWQ2Zjk2YWFkNDEwZjU1ZGFmNDViYWYwZGZkYTBiN2RmZjg4MGQ",
+        title:"realme C25Y 64 GB, 4 GB RAM, Glacier Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/OnePlus-9-SmartPhones-491947285-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2NDgwOHxpbWFnZS9qcGVnfGltYWdlcy9oMGEvaGFiLzk4NjkxMjYxMzk5MzQuanBnfGQ5ZmU4MTc4MjQ4Y2FhOTM3YzM0NTk5MjkwODg3MmQ3MWE3MTE4YTA2YjllMzY3ZDdjY2M1NGYyZDNlNzc5MmU",
+        title:"Motorola Moto Edge 30 5G 128GB, 6GB RAM, Meteor Grey, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/REDMI-NOTE-11-Mobile-Phone-492849123-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMjgzMHxpbWFnZS9qcGVnfGltYWdlcy9oYTIvaDNmLzk4MjAyOTczMzA3MTguanBnfDBmNDZmOWMzYTkwNTdlOGIyNWQyMzRhOWFkODRiZTg4MGFlZGUxYWQ4MzEyYmNiZTY2MTVjYTcxOTAzOTM2Yjk",
+        title:"Vivo X80 5G 128 GB, 8 GB RAM, Cosmic Black, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/OnePlus-Nord-CE-2-Lite-5G-Mobile-Phones-492850038-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2NDk5OXxpbWFnZS9wbmd8aW1hZ2VzL2g1MC9oOWIvOTg3Mzk3MDk4NzAzOC5wbmd8NjkzYjI5MDZjOWUzZjQ3N2Q4NDI4OWE3NDUxMmIzZjBkNDdjMGMwYTg1MGNhNjI2YTRjMWRmZjQ0ZjVlOWNjMw",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+]
+let battery=[
+    {
+        image:"https://www.reliancedigital.in/medias/OnePlus-10-Pro-Smartphone-492849811-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2ODg3NnxpbWFnZS9qcGVnfGltYWdlcy9oMTkvaDY4Lzk4ODIxMDU5MDUxODIuanBnfGRmNWU1MzljZDZjZmE1ZDNlNzJmNTFhOWEzNjg0MTM0OTQ2ODg4ZWFiZDE2NjYwOGI1YzRjZDExN2QzNDYwNGQ",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Oppo-K10-Smartphone-492849759-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2NTYxNHxpbWFnZS9qcGVnfGltYWdlcy9oMTQvaDNmLzk4NzAzMzM1NzUxOTguanBnfDdmNzBhMDNkMDJmZGE1MTBlYzc2MWJhYzQ1OWY0NGFjYzNmYjA5Y2IwNDVhZjQxNGY5ZmFkNmNiZTE4OTYxMDI",
+        title:"realme C25Y 64 GB, 4 GB RAM, Glacier Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Realme-RMX3491-GSMHANDSETSANDROID-492575026-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNjIyNXxpbWFnZS9qcGVnfGltYWdlcy9oZTUvaDhjLzk4MTU4MTkyMjMwNzAuanBnfDBjZjk5MDFkZTE4ZmY1NGVkNzA2Yjc3ZjdhNTI3ZTMwYmY3YmNkY2NjMGJiYTViMmEzNDg2ZDVmNTBmMDZmYjM",
+        title:"Motorola Moto Edge 30 5G 128GB, 6GB RAM, Meteor Grey, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/REDMI-NOTE-11-Mobile-Phone-492849123-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMjgzMHxpbWFnZS9qcGVnfGltYWdlcy9oYTIvaDNmLzk4MjAyOTczMzA3MTguanBnfDBmNDZmOWMzYTkwNTdlOGIyNWQyMzRhOWFkODRiZTg4MGFlZGUxYWQ4MzEyYmNiZTY2MTVjYTcxOTAzOTM2Yjk",
+        title:"Vivo X80 5G 128 GB, 8 GB RAM, Cosmic Black, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+    {
+        image:"https://www.reliancedigital.in/medias/Apple-Iphone12-Mobilephones-491901533-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3NzE5fGltYWdlL2pwZWd8aW1hZ2VzL2g3Ny9oNzEvOTg2MzgzNTU4MjQ5NC5qcGd8NDEzYzUzZjg1YmE5ZmVjNDYyNjMxNmY4ODRlNjIyOGM2MDQ1NzRiNGFkYmZjMTlmOTUxMjIzY2MyMWFlMmJlMA",
+        title:"Motorola G51 5G 64 GB, 4 GB RAM, Indigo Blue, Mobile Phone",
+        price:"12,999",
+        actualprice:"17,999",
+        save:"50%"
+    },
+]
+let gphones=document.getElementById("gphones")
+const showGphones=(Gphones)=>{
+    Gphones.forEach(function(ele){
+        let div=document.createElement("div")
+        div.setAttribute("class","phones_div")
+        let div2=document.createElement("div")
+        div2.setAttribute("class","phones_img")
+        div2.addEventListener("click",()=>{
+            location.href="showdata.html"
+        })
+    let image=document.createElement("img")
+    image.src=ele.image
+    image.setAttribute("class","image")
+    let title=document.createElement("h3")
+    title.innerText=ele.title
+    title.setAttribute("class","phone_title")
+    let price=document.createElement("p")
+    price.innerText=`Deal Price-${ele.price}`
+    price.setAttribute("class","deal_price")
+    let actualprice=document.createElement("p")
+    actualprice.innerText=`M.R.P:${ele.actualprice}`
+    actualprice.setAttribute("class","actualprice")
+    let save=document.createElement("p")
+    save.innerText=`You Save:${ele.save}`
+    save.setAttribute("class","yousave")
+    let offer=document.createElement("button")
+    offer.innerText="Offer Avalaible"
+    offer.setAttribute("class","offer")
+    div2.append(image)
+    div.append(div2,title,price,actualprice,save,offer)
+    gphones.append(div)
+    })
+    
+}
+showGphones(Gphones)
+
+let bestcamera=document.getElementById("best_camera")
+const showbestCamera=(bestCamera)=>{
+    bestCamera.forEach(function(ele){
+        let div=document.createElement("div")
+        div.setAttribute("class","phones_div")
+        let div2=document.createElement("div")
+        div2.setAttribute("class","phones_img")
+    let image=document.createElement("img")
+    image.src=ele.image
+    image.setAttribute("class","image")
+    let title=document.createElement("h3")
+    title.innerText=ele.title
+    title.setAttribute("class","phone_title")
+    let price=document.createElement("p")
+    price.innerText=`Deal Price-${ele.price}`
+    price.setAttribute("class","deal_price")
+    let actualprice=document.createElement("p")
+    actualprice.innerText=`M.R.P:${ele.actualprice}`
+    actualprice.setAttribute("class","actualprice")
+    let save=document.createElement("p")
+    save.innerText=`You Save:${ele.save}`
+    save.setAttribute("class","yousave")
+    let offer=document.createElement("button")
+    offer.innerText="Offer Avalaible"
+    offer.setAttribute("class","offer")
+    div2.append(image)
+    div.append(div2,title,price,actualprice,save,offer)
+    bestcamera.append(div)
+    })
+    
+}
+showbestCamera(bestCamera)
+
+let affordable_div=document.getElementById("affordable")
+const affordable=(affordabledata)=>{
+    affordabledata.forEach(function(ele){
+        let div=document.createElement("div")
+        div.setAttribute("class","phones_div")
+        let div2=document.createElement("div")
+        div2.setAttribute("class","phones_img")
+    let image=document.createElement("img")
+    image.src=ele.image
+    image.setAttribute("class","image")
+    let title=document.createElement("h3")
+    title.innerText=ele.title
+    title.setAttribute("class","phone_title")
+    let price=document.createElement("p")
+    price.innerText=`Deal Price-${ele.price}`
+    price.setAttribute("class","deal_price")
+    let actualprice=document.createElement("p")
+    actualprice.innerText=`M.R.P:${ele.actualprice}`
+    actualprice.setAttribute("class","actualprice")
+    let save=document.createElement("p")
+    save.innerText=`You Save:${ele.save}`
+    save.setAttribute("class","yousave")
+    let offer=document.createElement("button")
+    offer.innerText="Offer Avalaible"
+    offer.setAttribute("class","offer")
+    div2.append(image)
+    div.append(div2,title,price,actualprice,save,offer)
+    affordable_div.append(div)
+    })
+    
+}
+affordable(affordabledata)
+
+let gaming_div=document.getElementById("gaming")
+const gamingData=(gaming)=>{
+    gaming.forEach(function(ele){
+        let div=document.createElement("div")
+        div.setAttribute("class","phones_div")
+        let div2=document.createElement("div")
+        div2.setAttribute("class","phones_img")
+    let image=document.createElement("img")
+    image.src=ele.image
+    image.setAttribute("class","image")
+    let title=document.createElement("h3")
+    title.innerText=ele.title
+    title.setAttribute("class","phone_title")
+    let price=document.createElement("p")
+    price.innerText=`Deal Price-${ele.price}`
+    price.setAttribute("class","deal_price")
+    let actualprice=document.createElement("p")
+    actualprice.innerText=`M.R.P:${ele.actualprice}`
+    actualprice.setAttribute("class","actualprice")
+    let save=document.createElement("p")
+    save.innerText=`You Save:${ele.save}`
+    save.setAttribute("class","yousave")
+    let offer=document.createElement("button")
+    offer.innerText="Offer Avalaible"
+    offer.setAttribute("class","offer")
+    div2.append(image)
+    div.append(div2,title,price,actualprice,save,offer)
+    gaming_div.append(div)
+    })
+    
+}
+gamingData(gaming)
+
+let battery_div=document.getElementById("battery")
+const batteryData=(battery)=>{
+    battery.forEach(function(ele){
+        let div=document.createElement("div")
+        div.setAttribute("class","phones_div")
+        let div2=document.createElement("div")
+        div2.setAttribute("class","phones_img")
+    let image=document.createElement("img")
+    image.src=ele.image
+    image.setAttribute("class","image")
+    let title=document.createElement("h3")
+    title.innerText=ele.title
+    title.setAttribute("class","phone_title")
+    let price=document.createElement("p")
+    price.innerText=`Deal Price-${ele.price}`
+    price.setAttribute("class","deal_price")
+    let actualprice=document.createElement("p")
+    actualprice.innerText=`M.R.P:${ele.actualprice}`
+    actualprice.setAttribute("class","actualprice")
+    let save=document.createElement("p")
+    save.innerText=`You Save:${ele.save}`
+    save.setAttribute("class","yousave")
+    let offer=document.createElement("button")
+    offer.innerText="Offer Avalaible"
+    offer.setAttribute("class","offer")
+    div2.append(image)
+    div.append(div2,title,price,actualprice,save,offer)
+    battery_div.append(div)
+    })
+    
+}
+batteryData(battery)
