94337a70bfce278bb6e5f4f261cf11752dfdd330
Suprajna <suprajnaks@gmail.com>
2022-09-29 19:52:34 +0530

    file saved

--- homepage/index.js
+++ homepage/index.js
@@ -48,3 +48,32 @@ function secondslideShow(){
 
 }
 secondslideShow()
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+let slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+
+function currentSlides(n){
+    showSlides(slideIndex = n)
+}
